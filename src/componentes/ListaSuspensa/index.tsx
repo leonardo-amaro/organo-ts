ff8657168dc91556840303910a9022868a790526
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-05-05 10:33:59 -0300

    migrando ListaSuspensa

---
+++ src/componentes/ListaSuspensa/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './ListaSuspensa.css'
+
+interface ListaSuspensaProps {
+    label: string
+    aoAlterado: (valor: string) => void
+    required: boolean
+    valor: string
+    itens: string[]
+}
+
+const ListaSuspensa = (props: ListaSuspensaProps) => {
+    return (
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select 
+                onChange={(evento) => props.aoAlterado(evento.target.value)} 
+                required={props.required} 
+                value={props.valor}
+            >
+                <option value=""></option>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
